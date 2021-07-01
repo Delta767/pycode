5b7c99bf62bf188b633c04672de49817e28a3015
Delta767 <86267862+Delta767@users.noreply.github.com>
2021-07-01 12:03:21 -0700

    Add files via upload

---
+++ initals.py
@@ -0,0 +1,26 @@
+# initals.py
+print("    A     W             W X   X")
+print("   A A     W     W     W   X X")
+print("  A   A     W   W W   W     X")
+print(" A A A A     W W   W W     X X")
+print("A       A     W     W     X   X")
+
+#advanced
+stars = "* * * * * * * * * * * * * * * * * *"
+front = " * "
+end = " * "
+line1 = "\t     A     W             W X   X \n"
+line2 = "\t    A A     W     W     W   X X \n"
+line3 = "\t   A   A     W   W W   W     X \n"
+line4 = "\t  A A A A     W W   W W     X X \n"
+line5 = "\t A       A     W     W     X   X \n"
+print(stars)
+print(line1)
+print(line2)
+print(line3)
+print(line4)
+print(front+line5+end)
+print(stars)
+
+#sudo apt install speedtest-cli^C
+#speedtest
